2dbcb855ab786f4137b6edcab2c0bada74e1e8cd
supakorn.s <supakorn.s@kbtg.tech>
2021-03-01 15:42:34 +0700

    add jacoco

--- src/test/java/com/example/kbtg/DemoServiceTest.java
+++ src/test/java/com/example/kbtg/DemoServiceTest.java
@@ -13,7 +13,7 @@ public class DemoServiceTest {
 	@DisplayName("ในการทำงานต้อง random ได้ค่า 5")
 	public void random_5() {
 		DemoService demoService = new DemoService();
-		demoService.setRandom(new Random5());
+		demoService.setRandom(new MockRandom(5));
 		String actualResult = demoService.generateData("somkiat");
 		assertEquals("somkiat5", actualResult);
 	}
@@ -22,40 +22,34 @@ public class DemoServiceTest {
 	@DisplayName("ในการทำงานต้อง random 3 ได้ Exception")
 	public void random_3() {
 		DemoService demoService = new DemoService();
-		demoService.setRandom(new Random3());
+		demoService.setRandom(new MockRandom(3));
 		Exception exception = assertThrows(RuntimeException.class, () -> {
 			demoService.generateData("somkiat");
-		}, "Invalid number with 3");
+		});
+		assertEquals("Invalid number with 3", exception.getMessage());
 	}
 	
 	@Test
 	@DisplayName("ในการทำงานต้อง random 9 ได้ Exception")
 	public void random_9() {
 		DemoService demoService = new DemoService();
-		demoService.setRandom(new Random9());
-		Exception exception = assertThrows(RuntimeException.class, () -> {
+		demoService.setRandom(new MockRandom(9));
+		assertThrows(RuntimeException.class, () -> {
 			demoService.generateData("somkiat");
 		}, "Invalid number with 9");
 	}
 }
 
-class Random5 extends Random {
-	@Override
-	public int nextInt(int bound) {
-		return 5;
+class MockRandom extends Random {
+	
+	private final int result;
+	
+	public MockRandom(int result) {
+		this.result = result;
 	}
-}
-
-class Random3 extends Random {
+	
 	@Override
 	public int nextInt(int bound) {
-		return 3;
-	}
-}
-
-class Random9 extends Random {
-	@Override
-	public int nextInt(int bound) {
-		return 9;
+		return result;
 	}
 }
